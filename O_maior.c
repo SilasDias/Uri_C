c1152b459c888a10a46d3e6ead7969f68eb1caea
SilasDias <silasdjd@gmail.com>
2019-11-16 16:18:45 -0300

    Ok

---
+++ O_maior.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  int b;
+  int c;
+  int maiorAB;
+
+  scanf("%d%d%d", &a, &b, &c);
+
+  maiorAB = (a + b + abs(a-b))/2;
+  maiorAB = (maiorAB + c + abs(maiorAB - c))/2;
+  printf("%d eh maior\n", maiorAB);
+
+  return 0;
+}
