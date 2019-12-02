2a01323e7f44e1c5163d8f74ef390665bed34967
SilasDias <silasdjd@gmail.com>
2019-12-02 10:03:57 -0300

    Ok

--- Intervalo.c
+++ Intervalo.c
@@ -6,25 +6,28 @@ int main()
 
   scanf("%lf", &number);
 
-  if (number > 0.0 && number <= 25.0000)
+  if (number < 0 || number > 100)
   {
-    printf("Intervalo [0,25]\n");
-  }
-  else if (number >= 25.0001 && number <= 50.0000)
-  {
-    printf("Intervalo [25,50]\n");
-  }
-  else if (number >= 50.0001 && number <= 75.0000)
-  {
-    printf("Intervalo [50,75]\n");
-  }
-  else if (number >= 75.0001 && number <= 100.0000)
-  {
-    printf("Intervalo [75,100]\n");
+    printf("Fora de intervalo\n");
   }
   else
   {
-    printf("Fora de intervalo\n");
+    if (number >= 0 && number <= 25)
+      {
+        printf("Intervalo [0,25]\n");
+        }
+        else if (number > 25 && number <= 50)
+          {
+            printf("Intervalo [25,50]\n");
+            }
+            else if (number > 50 && number <= 75)
+              {
+                printf("Intervalo [50,75]\n");
+                }
+                else
+                  {
+                    printf("Intervalo [75,100]\n");
+                  }
   }
 
   return 0;
