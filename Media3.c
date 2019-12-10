705b5cb0d07a156ebcee8a50e97c339a6756d74f
SilasDias <silasdjd@gmail.com>
2019-12-09 23:12:12 -0300

    versão final

--- Media3.c
+++ Media3.c
@@ -14,30 +14,36 @@ int main()
 
   media = ((n1 * 2) + (n2 * 3) + (n3 * 4) + (n4 * 1)) / 10;
   printf("Media: %.1f\n", media);
-
+    
+  exam = 0;
   if (media < 5)
   {
     printf("Aluno reprovado.\n");
+    media = 0;
   }
-  if (media >= 7)
+  else if (media >= 7)
   {
     printf ("Aluno aprovado.\n");
+    media = 0;
   }
-  else if(media > 5 || media < 6.9)
+  else if(media >= 5 && media <= 6.9)
   {
     printf("Aluno em exame.\n");
     scanf("%f", &exam);
     printf("Nota do exame: %.1f\n", exam);
-    mediaexam = (media + exam) / 2;
   }
-  else if (mediaexam >= 5)
+  
+  mediaexam = (media + exam) / 2;
+  
+  if (mediaexam >= 5 && mediaexam != 0)
   {
     printf("Aluno aprovado.\n");
-    printf("Media final: %.1f\n", media);
+    printf("Media final: %.1f\n", mediaexam);
   }
-  else
+  else if (mediaexam <= 4.9 && mediaexam != 0)
   {
     printf("Aluno reprovado.\n");
+    printf("Media final: %.1f\n", mediaexam);
   }
 
   return 0;
