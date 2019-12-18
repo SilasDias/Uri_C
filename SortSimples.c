1d4e27997dc30e81dda45580b9302c52e7dcfbde
SilasDias <silasdjd@gmail.com>
2019-12-17 22:51:38 -0300

    Concluido

--- SortSimples.c
+++ SortSimples.c
@@ -2,13 +2,46 @@
 
 int main()
 {
-  int a;
-  int b;
-  int c;
-
-  scanf("%d%d%d", &a, &b, &c);
-
-  if (a < b && a < c)
-
-  return 0;
+    int a;
+    int b;
+    int c;
+    
+    scanf("%d%d%d", &a, &b, &c);
+    
+    if (a < b && a < c )
+    {
+        if (b < c)
+        {
+            printf("%d\n%d\n%d\n", a, b, c);
+        }
+        else 
+        {
+            printf("%d\n%d\n%d\n", a, c, b);
+        }
+    }
+    else if (b  < a && b < c)
+    {
+        if (a < c)
+        {
+            printf("%d\n%d\n%d\n", b, a, c);
+        }
+        else
+        {
+            printf("%d\n%d\n%d\n", b, c, a);
+        }
+    }
+    else if (c < a && c < b)
+    {
+        if (a < b)
+        {
+            printf("%d\n%d\n%d\n", c, a, b);
+        }
+        else
+        {
+            printf("%d\n%d\n%d\n", c, b, a);
+        }
+    }
+    printf("\n%d\n%d\n%d\n", a, b, c);
+    
+    return 0;
 }
