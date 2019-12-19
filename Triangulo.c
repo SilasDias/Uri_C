45299dc3920935ae452fe28ce24d5f2ff403991a
SilasDias <silasdjd@gmail.com>
2019-12-19 19:10:32 -0300

    Concluido

--- Triangulo.c
+++ Triangulo.c
@@ -8,5 +8,14 @@ int main()
 
   scanf("%f%f%f", &a, &b, &c);
 
+  if((b-c) < a && a < (b+c) && (a-c) < b && b < (a+c) && (a-b) < c && c < (a+b))
+  {
+    printf("Perimetro = %.1f\n", (a + b + c));
+  }
+  else 
+  {
+    printf("Area = %.1f\n", (((a + b) * c) / 2));
+  }
+
   return 0;
 }
