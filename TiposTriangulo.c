a08bc3582ea6e829e24f78b38d7818e247b403e5
SilasDias <silasdjd@gmail.com>
2020-01-06 20:25:43 -0300

    Estrutura

---
+++ TiposTriangulo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  float a;
+  float b;
+  float c;
+
+  scanf("%f%f%f", &a, &b, &c);
+
+  if (a > b && a > c)
+  {
+    if (b > c)
+    {
+
+
+  return 0;
+}
