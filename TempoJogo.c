3210a83ca49c4e3fd8f253bee89244dff9059035
SilasDias <silasdjd@gmail.com>
2020-01-07 19:18:48 -0300

    Ok

--- TempoJogo.c
+++ TempoJogo.c
@@ -8,16 +8,17 @@ int main()
 
   scanf("%d%d", &inicio, &fim);
 
-  if (fim > inicio)
-  {
-    duracao = fim - inicio;
-  }
-  else
-  {
-    duracao = inicio - fim;
-  }
+  duracao = fim - inicio;
 
-  printf("O JOGO DUROU %d HORA(S)\n", duracao);
+  if (duracao >= 1 && duracao <= 24)
+  {
+    printf("O JOGO DUROU %d HORA(S)\n", duracao);
+  }
+  else if (duracao < 0 || duracao == 0)
+  {
+    duracao += 24;
+    printf("O JOGO DUROU %d HORA(S)\n", duracao);
+  }
 
   return 0;
 }
