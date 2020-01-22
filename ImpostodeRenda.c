d00da1430992e3cba02d50371f840f6985548967
SilasDias <silasdjd@gmail.com>
2020-01-21 22:42:26 -0300

    Done

--- ImpostodeRenda.c
+++ ImpostodeRenda.c
@@ -3,52 +3,22 @@
 int main()
 {
   float salario;
-  float ajuste;
-  int juros;
-  float total;
+  float imposto;
 
   scanf("%f", &salario);
 
-  if (salario >= 0 && salario <= 400.00)
+  if (salario >= 2000.01 && salario <= 3000.00)
             {
-                juros = 15;
-                ajuste = (salario * 15) / 100;
-                total = salario + ajuste;
-                printf("Novo salario: %.2f\n", total);
-                printf("Reajuste ganho: %.2f\n", ajuste);
-                printf("Em percentual: %d %\n", juros);
-            }else if (salario > 400.00 && salario <= 800.00)
+               imposto = (salario - 2000.0) * 0.08;
+               printf("R$ %.2f\n", imposto);
+            }else if (salario > 3000.01 && salario <= 4500.00)
             {
-                juros = 12;
-                ajuste = (salario * 12) / 100;
-                total = salario + ajuste;
-                printf("Novo salario: %.2f\n", total);
-                printf("Reajuste ganho: %.2f\n", ajuste);
-                printf("Em percentual: %d %\n", juros);
-            }else if (salario > 800.00 && salario <= 1200.00)
+              imposto = (salario - 3000.0) * 0.18 + (1000 * 0.08);
+              printf("R$ %.2f\n", imposto);
+            }else if (salario > 4500.00)
             {
-                juros = 8;
-                ajuste = (salario * 10) / 100;
-                total = salario + ajuste;
-                printf("Novo salario: %.2f\n", total);
-                printf("Reajuste ganho: %.2f\n", ajuste);
-                printf("Em percentual: %d %\n", juros);
-            }else if (salario > 1200.00 && salario <= 2000.00)
-            {
-                juros = 07;
-                ajuste = (salario * 07) / 100;
-                total = salario + ajuste;
-                printf("Novo salario: %.2f\n", total);
-                printf("Reajuste ganho: %.2f\n", ajuste);
-                printf("Em percentual: %d %\n", juros);
-            }else if (salario > 2000.00)
-            {
-                juros = 04;
-                ajuste = (salario * 04) / 100;
-                total = salario + ajuste;
-                printf("Novo salario: %.2f\n", total);
-                printf("Reajuste ganho: %.2f\n", ajuste);
-                printf("Em percentual: %d %\n", juros);
+              imposto = (salario - 4500) * 0.28 + (1500 * 0.18) + (1000 * 0.08);
+              printf("R$ %.2f\n", imposto);
             }else
             {
               printf("Isento\n");
