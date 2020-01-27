35edad93e092995171afcbc1a52074a9a01adee6
SilasDias <silasdjd@gmail.com>
2020-01-27 11:15:06 -0300

    Done

--- NumerosPositivos.c
+++ NumerosPositivos.c
@@ -2,4 +2,19 @@
 
 int main()
 {
+  float x;
+  int count;
+  int i;
+  
+  for(i =0; i < 6; i++)
+  {
+    scanf("%f", &x);
+    if(x > 0)
+      {
+        count ++;
+        }
+  }
+  printf("%d valores positivos\n", count);
 
+  return 0;
+}
